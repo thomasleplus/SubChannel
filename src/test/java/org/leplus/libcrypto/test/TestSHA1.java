b9d784c2811b1522549dba9b9df08f165642d0ac
Thomas Leplus <thomasleplus@users.noreply.github.com>
2016-11-30 15:22:35 -0800

    Adding license

--- src/test/java/org/leplus/libcrypto/test/TestSHA1.java
+++ src/test/java/org/leplus/libcrypto/test/TestSHA1.java
@@ -1,4 +1,20 @@
-/* $Id: TestSHA1.java,v 1.1 2003/03/22 09:44:44 leplusth Exp $ */
+/*
+ * SubChannel - A study on subliminal channels in DSA algorithm.
+ * Copyright (C) 2016 Thomas Leplus
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 package org.leplus.libcrypto.test;
 
